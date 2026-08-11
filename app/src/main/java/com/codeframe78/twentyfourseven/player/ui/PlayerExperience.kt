78e5278ea2d2853cd43d37ab8516de43d766e0f1
codeframe78 <codeframe@gmail.com>
2026-08-11 15:17:40 -0700

    Use reclaimed inner player space

--- app/src/main/java/com/codeframe78/twentyfourseven/player/ui/PlayerExperience.kt
+++ app/src/main/java/com/codeframe78/twentyfourseven/player/ui/PlayerExperience.kt
@@ -87,7 +87,7 @@ import com.codeframe78.twentyfourseven.player.ui.theme.stationPalette
 private val ExpandedPlayerBreakpoint = 840.dp
 private val CompactPlayerReservedHeight = 420.dp
 private val MinimumCompactArtworkSize = 140.dp
-private val MaximumCompactArtworkSize = 320.dp
+private val MaximumCompactArtworkSize = 352.dp
 private val CompactPlayerNoScrollHeight = 720.dp
 private val SleepTimerPresetsMinutes = listOf(15, 30, 45, 60, 90)
 
