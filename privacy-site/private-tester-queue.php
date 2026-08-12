177651e27de316fe12690450ebb343f8f88b4b1f
codeframe78 <codeframe@gmail.com>
2026-08-12 12:30:35 -0700

    fix(site): support queue PHP lint runtime

--- privacy-site/private-tester-queue.php
+++ privacy-site/private-tester-queue.php
@@ -126,7 +126,7 @@ function validCsrf(): bool
         && hash_equals($_SESSION['csrf'], $_POST['csrf']);
 }
 
-function e(string|null $value): string
+function e(?string $value): string
 {
     return htmlspecialchars($value ?? '', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
 }
