dcdabe2e244b80795b39f7d715008a70b1bd1fda
codeframe78 <codeframe@gmail.com>
2026-08-19 15:34:51 -0700

    fix: advance Cast test build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.codeframe78.twentyfourseven.player"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.1.0-alpha02"
+        versionCode = 7
+        versionName = "0.1.0-alpha04"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["appLabel"] = "24Seven.FM Player"
     }
