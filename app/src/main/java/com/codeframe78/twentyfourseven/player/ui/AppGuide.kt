3cee8f42dd36b648cb7e001d986ddafc3f57906d
codeframe78 <codeframe@gmail.com>
2026-08-25 04:18:44 -0700

    fix: compile app guide state

--- app/src/main/java/com/codeframe78/twentyfourseven/player/ui/AppGuide.kt
+++ app/src/main/java/com/codeframe78/twentyfourseven/player/ui/AppGuide.kt
@@ -18,7 +18,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
